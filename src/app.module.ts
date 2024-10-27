c38f799dcd2e3eeb259a86c1c6b02310735de59d
Doyun <materokatti@gmail.com>
2024-10-28 08:54:11 +0900

    fix : import path name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { CommandHandler } from '@nestjs/cqrs';
-import { QueryHandler } from '@nestjs/cqrs';
+import { CommandHandlers } from './commands/handlers';
+import { QueryHandlers } from './queries/handlers';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [CqrsModule],
   controllers: [AppController],
-  providers: [AppService, ...CommandHandler, ...QueryHandler],
+  providers: [AppService, ...CommandHandlers, ...QueryHandlers],
 })
 export class AppModule { }
